0f07c9a87d656c742e41e7528abb6e204a1a1e0d
prophe <1172782111@qq.com>
2024-07-23 23:55:54 +0800

    add support for deepseek-coder-33b-instruct

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ supported_model_list = ['Qwen1.5-14B-Chat', 'Qwen1.5-32B-Chat', 'Qwen1.5-72B-Cha
                         'llama-3-70b-instruct', 'llama-3-8b-instruct',
                         'Phi-3-mini-128k-instruct', 'Phi-3-medium-128k-instruct',
                         'codegemma-1.1-7b-it', 'Mixtral-8x7B-Instruct-v0.1',
-                        'DeepSeek-Coder-V2-Lite-Instruct', 'DeepSeek-Coder-V2-Instruct']
+                        'DeepSeek-Coder-V2-Lite-Instruct', 'DeepSeek-Coder-V2-Instruct',
+                        'deepseek-coder-33b-instruct']
 
 suffix = ["", "wo_caller_local_", "wo_caller_global_", "wo_callee_local_", "wo_callee_global_",
           "wo_local_", "wo_global_", "wo_all_"]
